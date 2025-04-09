0c1c6d05ae534adcd450de35edc3c7c8a274de89
Konstantine Machavariani <kotiko2004@gmail.com>
2025-04-09 16:50:44 +0400

    Refactor Square.java

--- src/com/kotikom/chess/view/Square.java
+++ src/com/kotikom/chess/view/Square.java
@@ -4,84 +4,79 @@ import com.kotikom.chess.piece.Piece;
 
 import java.awt.Color;
 import java.awt.Graphics;
-
 import javax.swing.JComponent;
 import javax.swing.BorderFactory;
 
 public class Square extends JComponent {
-    private final Board b;
-    
+    private final Board board;
     private final int color;
-    private Piece occupyingPiece;
-    private boolean displayPiece;
-    
     private final int xNum;
     private final int yNum;
-    
-    public Square(Board b, int c, int xNum, int yNum) {
-        
-        this.b = b;
-        this.color = c;
+    private Piece occupyingPiece;
+    private boolean displayPiece;
+
+    public Square(Board b, int color, int xNum, int yNum) {
+        this.board = b;
+        this.color = color;
         this.displayPiece = true;
         this.xNum = xNum;
         this.yNum = yNum;
-        
-        
+
         this.setBorder(BorderFactory.createEmptyBorder());
     }
 
     public Piece getOccupyingPiece() {
         return occupyingPiece;
     }
-    
+
     public boolean isOccupied() {
         return (this.occupyingPiece != null);
     }
-    
+
     public int getXNum() {
         return this.xNum;
     }
-    
+
     public int getYNum() {
         return this.yNum;
     }
-    
+
     public void setDisplay(boolean v) {
         this.displayPiece = v;
     }
-    
+
     public void put(Piece p) {
         this.occupyingPiece = p;
         p.setPosition(this);
     }
-    
+
     public void removePiece() {
         this.occupyingPiece = null;
     }
-    
+
     public void capture(Piece p) {
         Piece k = getOccupyingPiece();
-        if (k.getColor() == 0) b.Bpieces.remove(k);
-        if (k.getColor() == 1) b.Wpieces.remove(k);
+        if (k.getColor() == 0) board.Bpieces.remove(k);
+        if (k.getColor() == 1) board.Wpieces.remove(k);
         this.occupyingPiece = p;
     }
-    
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
+
         if (this.color == 1) {
-            g.setColor(new Color(221,192,127));
+            g.setColor(new Color(221, 192, 127));
         } else {
-            g.setColor(new Color(101,67,33));
+            g.setColor(new Color(101, 67, 33));
         }
-        
+
         g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
-        
-        if(occupyingPiece != null && displayPiece) {
+
+        if (occupyingPiece != null && displayPiece) {
             occupyingPiece.draw(g);
         }
     }
-    
+
     @Override
     public int hashCode() {
         int prime = 31;
@@ -90,5 +85,4 @@ public class Square extends JComponent {
         result = prime * result + yNum;
         return result;
     }
-    
 }
