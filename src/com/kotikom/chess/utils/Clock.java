d9515eb0d90d9680fb03c116d391639dbdab92be
Konstantine Machavariani <kotiko2004@gmail.com>
2025-04-09 16:48:22 +0400

    Refactor Clock.java

--- src/com/kotikom/chess/utils/Clock.java
+++ src/com/kotikom/chess/utils/Clock.java
@@ -4,18 +4,18 @@ public class Clock {
     private int hh;
     private int mm;
     private int ss;
-    
+
     public Clock(int hh, int mm, int ss) {
         this.hh = hh;
         this.mm = mm;
         this.ss = ss;
     }
-    
+
     public boolean outOfTime() {
         return (hh == 0 && mm == 0 && ss == 0);
     }
-    
-    public void decr() {
+
+    public void decrementSeconds() {
         if (this.mm == 0 && this.ss == 0) {
             this.ss = 59;
             this.mm = 59;
@@ -25,12 +25,8 @@ public class Clock {
             this.mm--;
         } else this.ss--;
     }
-    
+
     public String getTime() {
-        String fHrs = String.format("%02d", this.hh);
-        String fMins = String.format("%02d", this.mm);
-        String fSecs = String.format("%02d", this.ss);
-        String fTime = fHrs + ":" + fMins + ":" + fSecs;
-        return fTime;
+        return String.format("%02d:%02d:%02d", hh, mm, ss);
     }
 }
--- src/com/kotikom/chess/view/GameWindow.java
+++ src/com/kotikom/chess/view/GameWindow.java
@@ -107,7 +107,7 @@ public class GameWindow {
                     boolean turn = board.getTurn();
                     
                     if (turn) {
-                        whiteClock.decr();
+                        whiteClock.decrementSeconds();
                         wTime.setText(whiteClock.getTime());
                         
                         if (whiteClock.outOfTime()) {
@@ -125,7 +125,7 @@ public class GameWindow {
                             } else gameWindow.dispose();
                         }
                     } else {
-                        blackClock.decr();
+                        blackClock.decrementSeconds();
                         bTime.setText(blackClock.getTime());
                         
                         if (blackClock.outOfTime()) {
